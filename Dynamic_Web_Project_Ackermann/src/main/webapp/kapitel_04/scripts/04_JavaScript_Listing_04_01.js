fde48f4e830be20156c0bb5c920b1f87ca19792f
nils-boernsen-dev <nils.boernsen@gmail.com>
2022-05-17 02:40:21 +0200

    Initial commit nach Einrichtung eines lokalen Repositorys

---
+++ Dynamic_Web_Project_Ackermann/src/main/webapp/kapitel_04/scripts/04_JavaScript_Listing_04_01.js
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+(
+	function() {
+		alert('Hallo Welt');
+	}
+	)();
---
+++ Dynamic_Web_Project_Ackermann/src/main/webapp/kapitel_06/scripts/06_Datenformate_Listing_Bsp_01_DOMParser.js
@@ -0,0 +1,29 @@
+/**
+ * Beispiel von: https://docs.w3cub.com/dom/domparser/parsefromstring
+ */
+ 
+import '"C:\Program Files\nodejs\node_modules\dom-parser\"';
+
+console.info('Start of 06_Datenformate_Listing_Bsp_01_DOMParser.js');
+const parser = new DOMParser();
+
+const xmlString = "<warning>Beware of the tiger</warning>";
+let doc1 = parser.parseFromString(xmlString, "application/xml");
+// XMLDocument
+
+const svgString = "<circle cx=\"50\" cy=\"50\" r=\"50\"/>";
+const doc2 = parser.parseFromString(svgString, "image/svg+xml");
+// XMLDocument
+
+const htmlString = "<strong>Beware of the leopard</strong>";
+const doc3 = parser.parseFromString(htmlString, "text/html");
+// HTMLDocument
+
+console.log(doc1.documentElement.textContent)
+// "Beware of the tiger"
+
+console.log(doc2.firstChild.tagName);
+// "circle"
+
+console.log(doc3.body.firstChild.textContent);
+// "Beware of the leopard"
